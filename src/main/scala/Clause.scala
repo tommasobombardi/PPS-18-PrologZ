bdfcf395c5bcd3fd6511562357bb5119f55f4391
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-09 17:09:53 +0200

    completed clause implementation

--- src/main/scala/Clause.scala
+++ src/main/scala/Clause.scala
@@ -4,26 +4,36 @@ import Term.Term
 
 object Clause {
 
+  sealed trait Predicate { def name: String }
+
   sealed trait Clause { def toProlog: String }
-  sealed trait Fact extends Clause { def name: String; def args: List[Term]; def :-(facts: Fact*): Rule }
+  sealed trait Fact extends Clause { def name: String; def args: List[Term] }
   sealed trait Rule extends Clause { def head: Fact; def body: List[Fact] }
 
-  sealed trait Predicate { def name: String; def apply(args: Term*): Fact }
+  private case class PredicateImpl(override val name: String) extends Predicate
 
   private case class FactImpl(override val name: String, override val args: List[Term]) extends Fact {
-    override def :-(facts: Fact*): Rule = {
-      require(facts.nonEmpty, "Body of a rule must be not empty")
-      RuleImpl(this, facts.toList)
-    }
-    override def toProlog: String = name + "(" + args.map(t => t.toProlog).mkString(",") + ")."
+    override def toProlog: String = name + "(" + args.map(_.toProlog).mkString(",") + ")."
   }
-
   private case class RuleImpl(override val head: Fact, override val body: List[Fact]) extends Rule {
-    override def toProlog: String = head.toProlog.dropRight(1) + ":-" + body.map(f => f.toProlog.dropRight(1)).mkString(",") + "."
+    override def toProlog: String = head.toProlog.dropRight(1) + ":-" + body.map(_.toProlog.dropRight(1)).mkString(",") + "."
   }
 
-  private case class PredicateImpl(override val name: String) extends Predicate {
-    override def apply(args: Term*): Fact = FactImpl(name, args.toList)
+  implicit class RichPredicate(base: ValidationNel[IllegalArgumentException, Predicate]) {
+    def apply(args: ValidationNel[IllegalArgumentException, Term]*): ValidationNel[IllegalArgumentException, Fact] = {
+      val argsVal: ValidationNel[IllegalArgumentException, List[Term]] =
+        args.foldLeft(List.empty[Term].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc))
+      (base |@| argsVal)((predicate, args) => FactImpl(predicate.name, args))
+    }
+  }
+
+  implicit class RichFact(base: ValidationNel[IllegalArgumentException, Fact]) {
+    def :-(facts: ValidationNel[IllegalArgumentException, Fact]*): ValidationNel[IllegalArgumentException, Rule] = {
+      val factsVal: ValidationNel[IllegalArgumentException, List[Fact]] =
+        if(facts.nonEmpty) facts.foldLeft(List.empty[Fact].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc))
+        else new IllegalArgumentException("Body (list of facts) of the rule must be not empty").failureNel
+      (base |@| factsVal)((head, body) => RuleImpl(head, body))
+    }
   }
 
   object Predicate {
