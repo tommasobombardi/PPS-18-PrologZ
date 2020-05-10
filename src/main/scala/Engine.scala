2337acf1da97cc3ef2dfaac728958a706c8bf5e9
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-10 22:04:21 +0200

    used illegal argument exception instead of string

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -10,25 +10,25 @@ object Engine {
   def resetTheory(): Unit = theory = Nil
 
   def solve(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): Unit = validateProgram(theory, goals) match {
-    case Failure(err: NonEmptyList[String]) => err.foreach(println(_))
+    case Failure(err: NonEmptyList[IllegalArgumentException]) => err.foreach(e => println(e.getMessage))
     case Success(p) => (p._1 |+| p._2).foreach(c => println(c.toProlog)) // TO DO step by step
   }
   def solveAll(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): Unit = validateProgram(theory, goals) match {
-    case Failure(err: NonEmptyList[String]) => err.foreach(println(_))
+    case Failure(err: NonEmptyList[IllegalArgumentException]) => err.foreach(e => println(e.getMessage))
     case Success(p) => (p._1 |+| p._2).foreach(c => println(c.toProlog)) // TO DO
   }
 
-  private def validateProgram(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): ValidationNel[String, (List[Clause], List[Fact])] = {
-    val theoryReadableVal: List[ValidationNel[String, Clause]] = theory.zipWithIndex.map({
-      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => "Error in Clause " + (index + 1) + ": " + err.getMessage).failure
+  private def validateProgram(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): ValidationNel[IllegalArgumentException, (List[Clause], List[Fact])] = {
+    val theoryReadableVal: List[ValidationNel[IllegalArgumentException, Clause]] = theory.zipWithIndex.map({
+      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => new IllegalArgumentException("Error in Clause " + (index + 1) + ": " + err.getMessage)).failure
       case (Success(a), _) => a.successNel
     })
-    val goalsReadableVal: List[ValidationNel[String, Fact]] = goals.zipWithIndex.map({
-      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => "Error in Goal " + (index + 1) + ": " + err.getMessage).failure
+    val goalsReadableVal: List[ValidationNel[IllegalArgumentException, Fact]] = goals.zipWithIndex.map({
+      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => new IllegalArgumentException("Error in Goal " + (index + 1) + ": " + err.getMessage)).failure
       case (Success(a), _) => a.successNel
     })
-    (theoryReadableVal.foldLeft(List.empty[Clause].successNel[String])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc))
-    |@| goalsReadableVal.foldLeft(List.empty[Fact].successNel[String])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc)))((_, _))
+    (theoryReadableVal.foldLeft(List.empty[Clause].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc))
+    |@| goalsReadableVal.foldLeft(List.empty[Fact].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => el :: acc)))((_, _))
   }
 
 }
