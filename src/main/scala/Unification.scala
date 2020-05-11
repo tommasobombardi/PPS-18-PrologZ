bd593eee9c3cf81f824894d5dfb2f8b2c844db9f
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-11 21:47:12 +0200

    added first substitution idea

---
+++ src/main/scala/Unification.scala
@@ -0,0 +1,17 @@
+import scalaz._
+import Scalaz._
+import Term.{Term, Variable}
+
+object Unification {
+
+  type Substitution = List[(Variable, Term)]
+
+  object Substitution {
+    def apply(args: (Variable, Term)*): Substitution = args.toList
+  }
+
+  implicit class RichSubstitution(base: Substitution) {
+    def toProlog: String = "{" + base.map(s => s._1.toProlog + "/" + s._2.toProlog).mkString(",") + "}"
+  }
+
+}
