f4f13d761ac4b5c915666dc9a4b208e40fe77478
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-16 14:36:46 +0200

    renamed unification object

--- src/main/scala/prologz/Substitution.scala
+++ src/main/scala/prologz/Substitution.scala
@@ -1,19 +1,17 @@
 package prologz
 
-import scalaz._
-import Scalaz._
 import prologz.Term._
 
-object Unification {
+private[prologz] object Substitution {
 
   type Substitution = List[(Variable, Term)]
 
-  object Substitution {
-    def apply(args: (Variable, Term)*): Substitution = args.toList
-  }
+  def apply(args: (Variable, Term)*): Substitution = args.toList
 
   implicit class RichSubstitution(base: Substitution) {
     def toProlog: String = "{" + base.map(s => s._1.toProlog + "/" + s._2.toProlog).mkString(",") + "}"
   }
+
+  implicit def fromTuple(arg: (Variable, Term)): Substitution = Substitution(arg)
 
 }
