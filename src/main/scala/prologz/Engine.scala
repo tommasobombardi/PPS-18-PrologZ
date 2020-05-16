9bdf9acd0df21fcd10e7d75b6b725b2c2ce42865
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-16 16:08:40 +0200

    removed all illegal argument exceptions

--- src/main/scala/prologz/Engine.scala
+++ src/main/scala/prologz/Engine.scala
@@ -3,34 +3,35 @@ package prologz
 import scalaz._
 import Scalaz._
 import prologz.Clause._
+import prologz.Term._
 
 object Engine {
 
-  private var theory: List[ValidationNel[IllegalArgumentException, Clause]] = Nil
+  private var theory: List[ValidationNel[String @@ InputError, Clause]] = Nil
 
-  def addTheory(clauses: ValidationNel[IllegalArgumentException, Clause]*): Unit = theory = theory |+| clauses.toList
+  def addTheory(clauses: ValidationNel[String @@ InputError, Clause]*): Unit = theory = theory |+| clauses.toList
   def resetTheory(): Unit = theory = Nil
 
-  def solve(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): Unit = validateProgram(theory, goals) match {
-    case Failure(err: NonEmptyList[IllegalArgumentException]) => err.foreach(e => println(e.getMessage))
+  def solve(theory: List[ValidationNel[String @@ InputError, Clause]], goals: List[ValidationNel[String @@ InputError, Fact]]): Unit = validateProgram(theory, goals) match {
+    case Failure(err: NonEmptyList[String @@ InputError]) => err.foreach(e => println(Tag.unwrap(e)))
     case Success(p) => (p._1 |+| p._2).foreach(c => println(c.toProlog)) // TO DO step by step
   }
-  def solveAll(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): Unit = validateProgram(theory, goals) match {
-    case Failure(err: NonEmptyList[IllegalArgumentException]) => err.foreach(e => println(e.getMessage))
+  def solveAll(theory: List[ValidationNel[String @@ InputError, Clause]], goals: List[ValidationNel[String @@ InputError, Fact]]): Unit = validateProgram(theory, goals) match {
+    case Failure(err: NonEmptyList[String @@ InputError]) => err.foreach(e => println(Tag.unwrap(e)))
     case Success(p) => (p._1 |+| p._2).foreach(c => println(c.toProlog)) // TO DO
   }
 
-  private def validateProgram(theory: List[ValidationNel[IllegalArgumentException, Clause]], goals: List[ValidationNel[IllegalArgumentException, Fact]]): ValidationNel[IllegalArgumentException, (List[Clause], List[Fact])] = {
-    val theoryReadableVal: List[ValidationNel[IllegalArgumentException, Clause]] = theory.zipWithIndex.map({
-      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => new IllegalArgumentException("Error in Clause " + (index + 1) + ": " + err.getMessage)).failure
+  private def validateProgram(theory: List[ValidationNel[String @@ InputError, Clause]], goals: List[ValidationNel[String @@ InputError, Fact]]): ValidationNel[String @@ InputError, (List[Clause], List[Fact])] = {
+    val theoryReadableVal: List[ValidationNel[String @@ InputError, Clause]] = theory.zipWithIndex.map({
+      case (Failure(nel: NonEmptyList[String @@ InputError]), index) => nel.map(err => InputError("Error in Clause " + (index + 1) + ": " + Tag.unwrap(err))).failure
       case (Success(a), _) => a.successNel
     })
-    val goalsReadableVal: List[ValidationNel[IllegalArgumentException, Fact]] = goals.zipWithIndex.map({
-      case (Failure(nel: NonEmptyList[IllegalArgumentException]), index) => nel.map(err => new IllegalArgumentException("Error in Goal " + (index + 1) + ": " + err.getMessage)).failure
+    val goalsReadableVal: List[ValidationNel[String @@ InputError, Fact]] = goals.zipWithIndex.map({
+      case (Failure(nel: NonEmptyList[String @@ InputError]), index) => nel.map(err => InputError("Error in Goal " + (index + 1) + ": " + Tag.unwrap(err))).failure
       case (Success(a), _) => a.successNel
     })
-    (theoryReadableVal.foldLeft(List.empty[Clause].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el))
-    |@| goalsReadableVal.foldLeft(List.empty[Fact].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el)))((_, _))
+    (theoryReadableVal.foldLeft(List.empty[Clause].successNel[String @@ InputError])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el))
+    |@| goalsReadableVal.foldLeft(List.empty[Fact].successNel[String @@ InputError])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el)))((_, _))
   }
 
 }
--- src/main/scala/prologz/Term.scala
+++ src/main/scala/prologz/Term.scala
@@ -29,13 +29,13 @@ object Term {
     def apply(name: String): ValidationNel[String @@ InputError, Functor] = {
       val nameVal1: ValidationNel[String @@ InputError, String] =
         if(name.nonEmpty) name.successNel
-        else new IllegalArgumentException("An empty string is not valid to represent a compound term").failureNel
+        else InputError("An empty string is not valid to represent a compound term").failureNel
       val nameVal2: ValidationNel[String @@ InputError, String] =
         if(name.toCharArray.forall(_.isLetter)) name.successNel
-        else new IllegalArgumentException("String '" + name + "' is not valid to represent a compound term, because it doesn't contain only letters").failureNel
+        else InputError("String '" + name + "' is not valid to represent a compound term, because it doesn't contain only letters").failureNel
       val nameVal3: ValidationNel[String @@ InputError, String] =
         if(name.nonEmpty && name.charAt(0).isLower) name.successNel
-        else new IllegalArgumentException("String '" + name + "' is not valid to represent a compound term, because it doesn't start with a lowercase letter").failureNel
+        else InputError("String '" + name + "' is not valid to represent a compound term, because it doesn't start with a lowercase letter").failureNel
       (nameVal1 |@| nameVal2 |@| nameVal3)((name, _, _) => FunctorImpl(name))
     }
   }
@@ -43,8 +43,8 @@ object Term {
   implicit class FunctorRich(base: ValidationNel[String @@ InputError, Functor]) {
     def apply(args: ValidationNel[String @@ InputError, Term]*): ValidationNel[String @@ InputError, Term] = {
       val argsVal: ValidationNel[String @@ InputError, List[Term]] =
-        if(args.nonEmpty) args.foldLeft(List.empty[Term].successNel[IllegalArgumentException])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el))
-        else new IllegalArgumentException("Body (namely the list of arguments) of a compound term must be not empty").failureNel
+        if(args.nonEmpty) args.foldLeft(List.empty[Term].successNel[String @@ InputError])((accumulator, element) => (accumulator |@| element)((acc, el) => acc :+ el))
+        else InputError("Body (namely the list of arguments) of a compound term must be not empty").failureNel
       (base |@| argsVal)((functor, args) => StructImpl(functor.name, args))
     }
   }
@@ -52,10 +52,10 @@ object Term {
   implicit def fromString(name: String): ValidationNel[String @@ InputError, Term] = {
     val nameVal1: ValidationNel[String @@ InputError, String] =
       if(name.nonEmpty) name.successNel
-      else new IllegalArgumentException("An empty string is not valid to represent a term").failureNel
+      else InputError("An empty string is not valid to represent a term").failureNel
     val nameVal2: ValidationNel[String @@ InputError, String] =
       if(name.toCharArray.forall(_.isLetter)) name.successNel
-      else new IllegalArgumentException("String '" + name + "' is not valid to represent a term, because it doesn't contain only letters").failureNel
+      else InputError("String '" + name + "' is not valid to represent a term, because it doesn't contain only letters").failureNel
     (nameVal1 |@| nameVal2)((name, _) => if(name.charAt(0).isLower) AtomImpl(name) else VariableImpl(name))
   }
 
