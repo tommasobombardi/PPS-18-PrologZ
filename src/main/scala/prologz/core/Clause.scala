409198df4e3c5c6d31dca4faf6c8603deb7faee5
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-22 18:44:05 +0200

    added scaladoc to clause definition

--- src/main/scala/prologz/core/Clause.scala
+++ src/main/scala/prologz/core/Clause.scala
@@ -1,14 +1,46 @@
 package prologz.core
 
+import scalaz._
+import Scalaz._
+import prologz.core.Validation.{InputError, PzValidation}
+
+/** Prolog predicate */
+sealed trait Predicate
+
+/** Prolog clause */
 sealed trait Clause { def toProlog: String }
+
+/** Fact clause */
 sealed trait Fact extends Clause { def name: String; def args: List[Term] }
-sealed trait Rule extends Clause { def head: Fact; def body: List[Fact] }
 
 private[core] case class FactImpl(override val name: String, override val args: List[Term]) extends Fact {
   override def toProlog: String = name + "(" + args.map(_.toProlog).mkString(",") + ")."
 }
+
+/** Rule clause */
+sealed trait Rule extends Clause { def head: Fact; def body: List[Fact] }
+
 private[core] case class RuleImpl(override val head: Fact, override val body: List[Fact]) extends Rule {
   override def toProlog: String = head.toProlog.dropRight(1) + ":-" + body.map(_.toProlog.dropRight(1)).mkString(",") + "."
 }
 
-sealed trait Predicate
+/** Factory for [[String @@ Predicate]] instances */
+object Predicate {
+  /** Creates a predicate
+   *
+   *  @param name predicate name
+   *  @return predicate if there are no errors in name, errors list otherwise
+   */
+  def apply(name: String): PzValidation[String @@ Predicate] = {
+    val nameVal1: PzValidation[String] =
+      if(name.nonEmpty) name.successNel
+      else InputError("An empty string is not valid to represent a predicate").failureNel
+    val nameVal2: PzValidation[String] =
+      if(name.toCharArray.forall(_.isLetter)) name.successNel
+      else InputError("String '" + name + "' is not valid to represent a predicate, because it doesn't contain only letters").failureNel
+    val nameVal3: PzValidation[String] =
+      if(name.nonEmpty && name.charAt(0).isLower) name.successNel
+      else InputError("String '" + name + "' is not valid to represent a predicate, because it doesn't start with a lowercase letter").failureNel
+    (nameVal1 |@| nameVal2 |@| nameVal3)((name, _, _) => Tag[String, Predicate](name))
+  }
+}
--- src/main/scala/prologz/core/ClauseImplicits.scala
+++ src/main/scala/prologz/core/ClauseImplicits.scala
@@ -7,20 +7,7 @@ import prologz.core.Validation.{InputError, PzValidation}
 
 object ClauseImplicits {
 
-  object Predicate {
-    def apply(name: String): PzValidation[String @@ Predicate] = {
-      val nameVal1: PzValidation[String] =
-        if(name.nonEmpty) name.successNel
-        else InputError("An empty string is not valid to represent a predicate").failureNel
-      val nameVal2: PzValidation[String] =
-        if(name.toCharArray.forall(_.isLetter)) name.successNel
-        else InputError("String '" + name + "' is not valid to represent a predicate, because it doesn't contain only letters").failureNel
-      val nameVal3: PzValidation[String] =
-        if(name.nonEmpty && name.charAt(0).isLower) name.successNel
-        else InputError("String '" + name + "' is not valid to represent a predicate, because it doesn't start with a lowercase letter").failureNel
-      (nameVal1 |@| nameVal2 |@| nameVal3)((name, _, _) => Tag[String, Predicate](name))
-    }
-  }
+
 
   implicit class PredicateRich(base: PzValidation[String @@ Predicate]) {
     def apply(args: PzValidation[Term]*): PzValidation[Fact] = {
