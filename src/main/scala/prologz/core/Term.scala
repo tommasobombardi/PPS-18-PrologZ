a61619e80b5179729e481f91ad7a875e7b01c0ac
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-22 10:02:26 +0200

    start adding scaladoc

--- src/main/scala/prologz/core/Term.scala
+++ src/main/scala/prologz/core/Term.scala
@@ -7,17 +7,59 @@ import prologz.core.Validation.{InputError, PzValidation}
 
 object Term {
 
-  sealed trait Term { def toProlog: String }
-  sealed trait Atom[A] extends Term { def value: A }
-  sealed trait Struct extends Term { def name: String; def args: List[Term] }
-  sealed trait Variable extends Term { def name: String }
+  /**
+   * General representation of a prolog term
+   */
+  sealed trait Term {
+    /**
+     * @return corresponding prolog term
+     */
+    def toProlog: String
+  }
+
+  /**
+   * Representation of a constant prolog term
+   * @tparam A type of the constant prolog term
+   */
+  sealed trait Atom[A] extends Term {
+    /**
+     * @return value of the constant prolog term
+     */
+    def value: A
+  }
+
+  /**
+   * Representation of a compound prolog term
+   */
+  sealed trait Struct extends Term {
+    /**
+     * @return functor name of the compound prolog term
+     */
+    def name: String
+    /**
+     * @return list of terms of the compound prolog term
+     */
+    def args: List[Term]
+  }
+
+  /**
+   * Representation of a variable prolog term
+   */
+  sealed trait Variable extends Term {
+    /**
+     * @return name of the variable prolog term
+     */
+    def name: String
+  }
 
   private[core] case class AtomImpl[A](override val value: A) extends Atom[A] {
     override def toProlog: String = value.toString
   }
+
   private[core] case class StructImpl(override val name: String, override val args: List[Term]) extends Struct {
     override def toProlog: String = name + "(" + args.map(_.toProlog).mkString(",") + ")"
   }
+
   private[core] case class VariableImpl(override val name: String) extends Variable {
     override def toProlog: String = name
   }
