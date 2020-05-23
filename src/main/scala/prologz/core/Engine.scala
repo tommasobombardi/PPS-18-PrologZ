09e9afc64bad28f350149468a37cad2a2bd64bec
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-23 12:03:21 +0200

    created a custom object for tree management

--- src/main/scala/prologz/core/Engine.scala
+++ src/main/scala/prologz/core/Engine.scala
@@ -5,7 +5,7 @@ import Scalaz._
 import scala.io.StdIn.readLine
 import prologz.core.PrologImplicits._
 import prologz.core.Substitution._
-import prologz.core.Unification._
+import prologz.core.TreeManager.{initializePrologTree, searchPrologTree, showPrologTree}
 import prologz.core.Validation.{PzValidation, validateProgram}
 
 /** Prolog engine used to solve programs */
@@ -42,24 +42,6 @@ object Engine {
    */
   def solveAll(goals: PzValidation[Fact]*): Unit = solveProgram(theory, goals.toList, stepByStep = false)
 
-  private def findNextSolution(theory: List[Clause], tree: TreeLoc[(List[Clause], List[Fact], Substitution)]): TreeLoc[(List[Clause], List[Fact], Substitution)] =
-    if(tree.isRoot) navigatePrologTree(theory, tree) else navigatePrologTree(theory, tree.parent.get) // backtracking (in case of leaf node with valid solution)
-
-  @scala.annotation.tailrec
-  private def navigatePrologTree(theory: List[Clause], tree: TreeLoc[(List[Clause], List[Fact], Substitution)]): TreeLoc[(List[Clause], List[Fact], Substitution)] = tree.getLabel match {
-    case (clause :: otherClauses, goal :: otherGoals, subs) =>
-      var currentNode: TreeLoc[(List[Clause], List[Fact], Substitution)] = tree.setLabel(otherClauses, goal :: otherGoals, subs)
-      currentNode = clause.unify(goal, otherGoals).map(res => currentNode.insertDownLast((theory, res._2, subs |+| res._1).leaf)).getOrElse(currentNode)
-      navigatePrologTree(theory, currentNode)
-    case (_, _ :: _, _) if tree.parent.isDefined => navigatePrologTree(theory, tree.parent.get) // automatic backtracking (in case of leaf node without valid solution)
-    case (_, goals, subs) => tree.setLabel(Nil, goals, subs) // valid solution (in case of leaf node) or  execution completed (in case of root node)
-  }
-
-  /** Policy for tree drawing */
-  private implicit val showPrologTree: Show[(List[Clause], List[Fact], Substitution)] = Show.shows(el => {
-    el._2.map(_.toProlog.dropRight(1)).mkString(",") + " || " + el._3.getResult.toProlog
-  })
-
   private def solveProgram(theory: List[PzValidation[Clause]], goals: List[PzValidation[Fact]], stepByStep: Boolean): Unit = validateProgram(theory, goals) match {
     case Failure(err: NonEmptyList[String @@ InputError]) =>
       solved += 1
@@ -69,8 +51,8 @@ object Engine {
     case Success(p) =>
       solved += 1
       println("[PROLOG ENGINE] Resolution of program " + solved)
-      val tree: TreeLoc[(List[Clause], List[Fact], Substitution)] = (p._1, p._2, Substitution.base(p._2.getVariables)).leaf.loc
-        .doWhile(node => findNextSolution(p._1, node), node => {
+      val tree: TreeLoc[(List[Clause], List[Fact], Substitution)] = initializePrologTree(p._1, p._2)
+        .doWhile(node => searchPrologTree(p._1, node), node => {
           if(node.getLabel._2.isEmpty) {
             println("[PROLOG ENGINE] Available solution: " + node.getLabel._3.getResult.toProlog)
             println("[PROLOG ENGINE] Available solution: " + p._2.map(_.substitute(node.getLabel._3.getResult)).map(_.toProlog.dropRight(1)).mkString(","))
---
+++ src/main/scala/prologz/core/TreeManager.scala
@@ -0,0 +1,32 @@
+package prologz.core
+
+import scalaz._
+import Scalaz._
+import prologz.core.PrologImplicits._
+import prologz.core.Substitution._
+import prologz.core.Unification._
+
+private[core] object TreeManager {
+
+  def initializePrologTree(theory: List[Clause], goals: List[Fact]): TreeLoc[(List[Clause], List[Fact], Substitution)] =
+    (theory, goals, Substitution.base(goals.getVariables)).leaf.loc
+
+  def searchPrologTree(theory: List[Clause], tree: TreeLoc[(List[Clause], List[Fact], Substitution)]): TreeLoc[(List[Clause], List[Fact], Substitution)] =
+    if(tree.isRoot) navigatePrologTree(theory, tree) else navigatePrologTree(theory, tree.parent.get) // backtracking (in case of leaf node with valid solution)
+
+  @scala.annotation.tailrec
+  private def navigatePrologTree(theory: List[Clause], tree: TreeLoc[(List[Clause], List[Fact], Substitution)]): TreeLoc[(List[Clause], List[Fact], Substitution)] = tree.getLabel match {
+    case (clause :: otherClauses, goal :: otherGoals, subs) =>
+      var currentNode: TreeLoc[(List[Clause], List[Fact], Substitution)] = tree.setLabel(otherClauses, goal :: otherGoals, subs)
+      currentNode = clause.unify(goal, otherGoals).map(res => currentNode.insertDownLast((theory, res._2, subs |+| res._1).leaf)).getOrElse(currentNode)
+      navigatePrologTree(theory, currentNode)
+    case (_, _ :: _, _) if tree.parent.isDefined => navigatePrologTree(theory, tree.parent.get) // automatic backtracking (in case of leaf node without valid solution)
+    case (_, goals, subs) => tree.setLabel(Nil, goals, subs) // valid solution (in case of leaf node) or  execution completed (in case of root node)
+  }
+
+  /** Policy for tree drawing */
+  implicit val showPrologTree: Show[(List[Clause], List[Fact], Substitution)] = Show.shows(el => {
+    el._2.map(_.toProlog.dropRight(1)).mkString(",") + " || " + el._3.getResult.toProlog
+  })
+
+}
