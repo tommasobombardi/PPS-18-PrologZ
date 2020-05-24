195f246a1ccbe476d8176c968537b75475186386
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-24 22:41:37 +0200

    used matchers instead of assert

--- src/test/scala/prologz/resolution/ValidationSpec.scala
+++ src/test/scala/prologz/resolution/ValidationSpec.scala
@@ -3,38 +3,30 @@ package prologz.resolution
 import scalaz._
 import Scalaz._
 import org.scalatest.flatspec.AnyFlatSpec
-import prologz.dsl.{Clause, Fact}
-import prologz.resolution.Validation.{InputError, PzValidation, validateProgram}
+import org.scalatest.matchers.should.Matchers
+import prologz.resolution.Validation.{InputError, validateProgram}
 import prologz.utils.Utils
 
-class ValidationSpec extends AnyFlatSpec with Utils {
+class ValidationSpec extends AnyFlatSpec with Matchers with Utils {
 
   "The validate method" should "transform a valid program in its validated version" in {
-    val mulValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(mulTheoryNoErrors, mulGoalsNoErrors)
-    assert(mulValidation.isSuccess && mulValidation.getOrElse((Nil, Nil)) == (mulTheory, mulGoals))
-    val relValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(relTheoryNoErrors, relGoalsNoErrors)
-    assert(relValidation.isSuccess && relValidation.getOrElse((Nil, Nil)) == (relTheory, relGoals))
+    validateProgram(mulTheoryNoErrors, mulGoalsNoErrors)getOrElse((Nil, Nil)) shouldBe (mulTheory, mulGoals)
+    validateProgram(relTheoryNoErrors, relGoalsNoErrors).getOrElse((Nil, Nil)) shouldBe (relTheory, relGoals)
   }
 
   it should "detect if there are errors only in program theory and report them" in {
-    val mulValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(mulTheoryWithErrors, mulGoalsNoErrors)
-    assert(mulValidation.isFailure && mulValidation.swap.getOrElse(InputError("").wrapNel).size == 7) // 7 (7 + 0)
-    val relValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(relTheoryWithErrors, relGoalsNoErrors)
-    assert(relValidation.isFailure && relValidation.swap.getOrElse(InputError("").wrapNel).size == 3) // 3 (3 + 0)
+    validateProgram(mulTheoryWithErrors, mulGoalsNoErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 7 // 7 (7 + 0)
+    validateProgram(relTheoryWithErrors, relGoalsNoErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 3 // 3 (3 + 0)
   }
 
   it should "detect if there are errors only in program goals and report them" in {
-    val mulValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(mulTheoryNoErrors, mulGoalsWithErrors)
-    assert(mulValidation.isFailure && mulValidation.swap.getOrElse(InputError("").wrapNel).size == 4) // 4 (0 + 4)
-    val relValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(relTheoryNoErrors, relGoalsWithErrors)
-    assert(relValidation.isFailure && relValidation.swap.getOrElse(InputError("").wrapNel).size == 2) // 2 (0 + 2)
+    validateProgram(mulTheoryNoErrors, mulGoalsWithErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 4 // 4 (0 + 4)
+    validateProgram(relTheoryNoErrors, relGoalsWithErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 2 // 2 (0 + 2)
   }
 
   it should "detect if there are errors both in program theory and goals and report them" in {
-    val mulValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(mulTheoryWithErrors, mulGoalsWithErrors)
-    assert(mulValidation.isFailure && mulValidation.swap.getOrElse(InputError("").wrapNel).size == 11) // 11 (7 + 4)
-    val relValidation: PzValidation[(List[Clause], List[Fact])] = validateProgram(relTheoryWithErrors, relGoalsWithErrors)
-    assert(relValidation.isFailure && relValidation.swap.getOrElse(InputError("").wrapNel).size == 5) // 5 (3 + 2)
+    validateProgram(mulTheoryWithErrors, mulGoalsWithErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 11 // 11 (7 + 4)
+    validateProgram(relTheoryWithErrors, relGoalsWithErrors).swap.getOrElse(InputError("").wrapNel).size shouldBe 5 // 5 (3 + 2)
   }
 
 }
