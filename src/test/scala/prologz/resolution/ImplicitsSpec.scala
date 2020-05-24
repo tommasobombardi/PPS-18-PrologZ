42107ac03c0d955167988f4836edda60a35be0e0
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-24 16:26:42 +0200

    added implicit spec implementation

--- src/test/scala/prologz/resolution/ImplicitsSpec.scala
+++ src/test/scala/prologz/resolution/ImplicitsSpec.scala
@@ -1,7 +1,57 @@
 package prologz.resolution
 
 import org.scalatest.flatspec.AnyFlatSpec
+import prologz.resolution.Implicits.{RichFact, RichFactList, RichRule, RichTermList}
+import prologz.utils.TestUtils
 
-class ImplicitsSpec extends AnyFlatSpec {
+class ImplicitsSpec extends AnyFlatSpec with TestUtils {
+
+  "A term list" should "retrieve all the variables it contains" in {
+    assert(termList.getVariables == termListVariables)
+  }
+
+  it should "perform a renaming generating a correctly updated term list" in {
+    assert(termList.rename(variablesToRename) == termListRenamed)
+  }
+
+  it should "perform a substitution generating a correctly updated term list" in {
+    assert(termList.substitute(substitution) == termListSubstituted)
+  }
+
+  "A fact" should "retrieve all the variables it contains" in {
+    assert(fact.getVariables == factVariables)
+  }
+
+  it should "perform a renaming generating a correctly updated fact" in {
+    assert(fact.rename(variablesToRename) == factRenamed)
+  }
+
+  it should "perform a substitution generating a correctly updated fact" in {
+    assert(fact.substitute(substitution) == factSubstituted)
+  }
+
+  "A fact list" should "retrieve all the variables it contains" in {
+    assert(factList.getVariables == factListVariables)
+  }
+
+  it should "perform a renaming generating a correctly updated fact list" in {
+    assert(factList.rename(variablesToRename) == factListRenamed)
+  }
+
+  it should "perform a substitution generating a correctly updated fact list" in {
+    assert(factList.substitute(substitution) == factListSubstituted)
+  }
+
+  "A rule" should "retrieve all the variables it contains" in {
+    assert(rule.getVariables == ruleVariables)
+  }
+
+  it should "perform a renaming generating a correctly updated rule" in {
+    assert(rule.rename(variablesToRename) == ruleRenamed)
+  }
+
+  it should "perform a substitution generating a correctly updated rule" in {
+    assert(rule.substitute(substitution) == ruleSubstituted)
+  }
 
 }
--- src/test/scala/prologz/utils/TestUtils.scala
+++ src/test/scala/prologz/utils/TestUtils.scala
@@ -9,22 +9,24 @@ trait TestUtils {
   val variablesToRename: Set[Variable] = Set(VariableImpl("X"), VariableImpl("Y"),  VariableImpl("Z"))
   val substitution: Substitution = Substitution(VariableImpl("X") -> AtomImpl("x"), VariableImpl("Y") -> AtomImpl("y"))
 
-  val emptyTermList: List[Term] = Nil
   val termList: List[Term] = List(VariableImpl("X"), AtomImpl(0), StructImpl("s", List(VariableImpl("Y"), AtomImpl(1))), AtomImpl("a"))
   val termListRenamed: List[Term] = List(VariableImpl("X'"), AtomImpl(0), StructImpl("s", List(VariableImpl("Y'"), AtomImpl(1))), AtomImpl("a"))
   val termListSubstituted: List[Term] = List(AtomImpl("x"), AtomImpl(0), StructImpl("s", List(AtomImpl("y"), AtomImpl(1))), AtomImpl("a"))
+  val termListVariables: Set[Variable] = Set(VariableImpl("X"), VariableImpl("Y"))
 
   val fact: Fact = FactImpl("sampleFact", List(AtomImpl(0), StructImpl("p", List(VariableImpl("X"), VariableImpl("Z")))))
   val factRenamed: Fact = FactImpl("sampleFact", List(AtomImpl(0), StructImpl("p", List(VariableImpl("X'"), VariableImpl("Z'")))))
   val factSubstituted: Fact = FactImpl("sampleFact", List(AtomImpl(0), StructImpl("p", List(AtomImpl("x"), VariableImpl("Z")))))
+  val factVariables: Set[Variable] = Set(VariableImpl("X"), VariableImpl("Z"))
 
-  val emptyFactList: List[Fact] = Nil
   val factList: List[Fact] = List(fact, FactImpl("otherFact", termList))
   val factListRenamed: List[Fact] = List(factRenamed, FactImpl("otherFact", termListRenamed))
   val factListSubstituted: List[Fact] = List(factSubstituted, FactImpl("otherFact", termListSubstituted))
+  val factListVariables: Set[Variable] = Set(VariableImpl("X"), VariableImpl("Z"), VariableImpl("Y"))
 
   val rule: Rule = RuleImpl(FactImpl("ruleHead", List(VariableImpl("Z"), AtomImpl(2.5), AtomImpl("a"), VariableImpl("Y"))), factList)
   val ruleRenamed: Rule = RuleImpl(FactImpl("ruleHead", List(VariableImpl("Z'"), AtomImpl(2.5), AtomImpl("a"), VariableImpl("Y'"))), factListRenamed)
-  val ruleSubstituted: Rule = RuleImpl(FactImpl("ruleHead", List(VariableImpl("Z"), AtomImpl(2.5), AtomImpl("a"), AtomImpl("y"))), factList)
+  val ruleSubstituted: Rule = RuleImpl(FactImpl("ruleHead", List(VariableImpl("Z"), AtomImpl(2.5), AtomImpl("a"), AtomImpl("y"))), factListSubstituted)
+  val ruleVariables: Set[Variable] = Set(VariableImpl("Z"), VariableImpl("Y"), VariableImpl("X"))
 
 }
