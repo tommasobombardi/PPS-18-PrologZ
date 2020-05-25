b8a89728cc9d56cb5386472912f9989db1fcb1e9
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 16:50:37 +0200

    successfully completed substitution test

--- src/test/scala/prologz/resolution/SubstitutionSpec.scala
+++ src/test/scala/prologz/resolution/SubstitutionSpec.scala
@@ -5,7 +5,7 @@ import Scalaz._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import prologz.dsl.{AtomImpl, StructImpl, VariableImpl}
-import prologz.resolution.Substitution.{Substitution, substitutionMonoid}
+import prologz.resolution.Substitution.{RichSubstitution, Substitution, substitutionMonoid}
 
 class SubstitutionSpec extends AnyFlatSpec with Matchers {
 
@@ -13,9 +13,10 @@ class SubstitutionSpec extends AnyFlatSpec with Matchers {
   private val substitution2: Substitution = Map(VariableImpl("X") -> AtomImpl(1), VariableImpl("Y") -> StructImpl("s", List(VariableImpl("Z"))), VariableImpl("Z") -> VariableImpl("Y'"))
   private val substitution3: Substitution = Map(VariableImpl("Y'") -> AtomImpl(3), VariableImpl("Z") -> VariableImpl("X"))
 
+  private val expected123 = Substitution(VariableImpl("X") -> AtomImpl(1), VariableImpl("Y") -> StructImpl("s", List(AtomImpl(3))), VariableImpl("Z") -> AtomImpl(3))
   private val expected132 = Substitution(VariableImpl("X") -> AtomImpl(1), VariableImpl("Y") -> StructImpl("s", List(VariableImpl("Y'"))), VariableImpl("Z") -> AtomImpl(1))
-  private val expected23 = Substitution(VariableImpl("X") -> AtomImpl(1), VariableImpl("Y") -> StructImpl("s", List(AtomImpl(3))), VariableImpl("Z") -> AtomImpl(3)) // 123, 213, 231
-  private val expected32 = Substitution(VariableImpl("Y'") -> AtomImpl(3), VariableImpl("Z") -> AtomImpl(1)) // 312, 321
+  private val expected312 = Substitution(VariableImpl("Y'") -> AtomImpl(3), VariableImpl("Z") -> AtomImpl(1))
+  private val expected321 = Substitution(VariableImpl("Y'") -> AtomImpl(3), VariableImpl("Z") -> AtomImpl(1))
 
   "The substitution factory" should "create a valid substitution starting from tuples containing a variable and a term" in {
     Substitution((VariableImpl("X"), VariableImpl("X")), (VariableImpl("Y"), VariableImpl("Y")), (VariableImpl("Z"), VariableImpl("Z"))) shouldBe substitution1
@@ -27,13 +28,17 @@ class SubstitutionSpec extends AnyFlatSpec with Matchers {
     Substitution.base(Set(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"))) shouldBe substitution1
   }
 
-  "A valid substitution" should "be appended to other valid substitutions generating a valid composition" in {
-    substitution1 |+| substitution2 |+| substitution3 shouldBe expected23 // expected123 == expected23
+  "A valid substitution" should "be appended to other valid substitutions generating the proper composition" in {
+    substitution1 |+| substitution2 |+| substitution3 shouldBe expected123
     substitution1 |+| substitution3 |+| substitution2 shouldBe expected132
-    substitution2 |+| substitution1 |+| substitution3 shouldBe expected23 // expected213 == expected23
-    substitution2 |+| substitution3 |+| substitution1 shouldBe expected23 // expected231 == expected23
-    substitution3 |+| substitution1 |+| substitution2 shouldBe expected32 // expected312 == expected32
-    substitution3 |+| substitution2 |+| substitution1 shouldBe expected32 // expected321 == expected32
+    substitution3 |+| substitution1 |+| substitution2 shouldBe expected312
+    substitution3 |+| substitution2 |+| substitution1 shouldBe expected321
+  }
+
+  it should "retrieve an equivalent substitution without identity associations" in {
+    substitution1.getResult should have size 0
+    substitution2.getResult shouldBe substitution2
+    substitution3.getResult shouldBe substitution3
   }
 
 }
