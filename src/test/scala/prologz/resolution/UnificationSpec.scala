34b44ed4b228bd6dbc3d80419c09ea450084f9ca
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 11:11:48 +0200

    added utility values to unif spec

--- src/test/scala/prologz/resolution/UnificationSpec.scala
+++ src/test/scala/prologz/resolution/UnificationSpec.scala
@@ -4,16 +4,28 @@ import scalaz._
 import Scalaz._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import prologz.dsl.{Fact, Rule}
+import prologz.dsl.{AtomImpl, Fact, FactImpl, Rule, StructImpl, VariableImpl}
 import prologz.resolution.Unification.RichClause
 import prologz.utils.Utils
 
 class UnificationSpec extends AnyFlatSpec with Matchers with Utils {
 
-  private val mulTheoryFacts: List[Fact] = mulTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
-  private val mulTheoryRules: List[Rule] = mulTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
-  private val relTheoryFacts: List[Fact] = relTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
-  private val relTheoryRules: List[Rule] = relTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
+  private val mulTheoryFacts = mulTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
+  private val mulTheoryRules = mulTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
+  private val relTheoryFacts = relTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
+  private val relTheoryRules = relTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
+
+  private val otherMulGoal = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0), VariableImpl("Y")))
+  private val otherMulGoalRes = Substitution(VariableImpl("Y") -> AtomImpl(0))
+  private val mulGoalAfterOther = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0)))
+  private val mulGoalRes = Substitution(VariableImpl("Y") -> StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))))))))
+  private val otherMulGoalAfterFirst = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0), StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0)))))))))
+
+  private val otherRelGoal = FactImpl("father", List(VariableImpl("X"), VariableImpl("Y")))
+  private val otherRelGoalRes = Substitution(VariableImpl("X") -> AtomImpl("abraham"), VariableImpl("Y") -> AtomImpl("isaac"))
+  private val relGoalAfterOther = FactImpl("son", List(VariableImpl("abraham"), VariableImpl("isaac")))
+  private val relGoalRes = Substitution(VariableImpl("X") -> AtomImpl("isaac"), VariableImpl("Y") -> AtomImpl("abraham"))
+  private val otherRelGoalAfterFirst = FactImpl("father", List(VariableImpl("isaac"), VariableImpl("abraham")))
 
   "A theory fact" should "not unify to a goal with a different predicate name" in {
     for(theory <- mulTheoryFacts; goal <- mulGoals if theory.name != goal.name) theory.unify(goal, Nil) shouldBe None
@@ -26,6 +38,8 @@ class UnificationSpec extends AnyFlatSpec with Matchers with Utils {
   }
 
   it should "unify to a compatible goal retrieving the substitution and other goals after its application" in {
+    mulTheoryFacts
+
 
   }
 
