0be23a2710bc4d193e7ff460087afd88007f8ed6
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 20:17:52 +0200

    start implementing prolog tree tests

--- src/test/scala/prologz/resolution/PrologTreeSpec.scala
+++ src/test/scala/prologz/resolution/PrologTreeSpec.scala
@@ -1,7 +1,17 @@
 package prologz.resolution
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import utils.PrologSamples
 
-class PrologTreeSpec extends AnyFlatSpec {
+class PrologTreeSpec extends AnyFlatSpec with Matchers with PrologSamples {
+
+  "The prolog tree manager" should "correctly initialize the resolution tree for a program" in {
+
+  }
+
+  it should "search through the tree retrieving a leaf (valid solution) or the root (end of computation)" in {
+
+  }
 
 }
