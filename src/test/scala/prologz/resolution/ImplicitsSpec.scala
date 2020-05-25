37f7fc501a32da2f1798580da69bdb28ec2cd55f
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 17:52:22 +0200

    copied def from utils file

--- src/test/scala/prologz/resolution/ImplicitsSpec.scala
+++ src/test/scala/prologz/resolution/ImplicitsSpec.scala
@@ -1,10 +1,50 @@
 package prologz.resolution
 
 import org.scalatest.flatspec.AnyFlatSpec
+import prologz.dsl.{AtomImpl, Clause, FactImpl, RuleImpl, StructImpl, VariableImpl}
 import prologz.resolution.Implicits.{RichFact, RichFactList, RichRule, RichTermList}
 import prologz.utils.TestUtils
 
 class ImplicitsSpec extends AnyFlatSpec with TestUtils {
+
+  val mulTheoryVariables: List[Clause] = List(FactImpl("sum", List(VariableImpl("X"), AtomImpl(0), VariableImpl("X"))),
+    RuleImpl(FactImpl("sum", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), StructImpl("s", List(VariableImpl("Z"))))),
+      List(FactImpl("sum", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"))))),
+    FactImpl("mul", List(VariableImpl("X"), AtomImpl(0), AtomImpl(0))),
+    RuleImpl(FactImpl("mul", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), VariableImpl("Z"))),
+      List(FactImpl("mul", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("W"))), FactImpl("sum", List(VariableImpl("X"), VariableImpl("W"), VariableImpl("Z"))))))
+
+  val relTheoryVariables: List[Clause] = List(RuleImpl(FactImpl("son", List(VariableImpl("X"), VariableImpl("Y"))),
+      List(FactImpl("father", List(VariableImpl("Y"), VariableImpl("X"))), FactImpl("male", List(VariableImpl("X"))))),
+    FactImpl("father", List(AtomImpl("abraham"), AtomImpl("isaac"))), FactImpl("father", List(AtomImpl("terach"), AtomImpl("abraham"))),
+    FactImpl("male", List(AtomImpl("isaac"))), FactImpl("male", List(AtomImpl("abraham"))), FactImpl("male", List(AtomImpl("terach"))))
+
+
+  val mulTheoryRenamed: List[Clause] = List(FactImpl("sum", List(VariableImpl("X"), AtomImpl(0), VariableImpl("X"))),
+    RuleImpl(FactImpl("sum", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), StructImpl("s", List(VariableImpl("Z"))))),
+      List(FactImpl("sum", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"))))),
+    FactImpl("mul", List(VariableImpl("X"), AtomImpl(0), AtomImpl(0))),
+    RuleImpl(FactImpl("mul", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), VariableImpl("Z"))),
+      List(FactImpl("mul", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("W"))), FactImpl("sum", List(VariableImpl("X"), VariableImpl("W"), VariableImpl("Z"))))))
+
+  val relTheoryRenamed: List[Clause] = List(RuleImpl(FactImpl("son", List(VariableImpl("X"), VariableImpl("Y"))),
+    List(FactImpl("father", List(VariableImpl("Y"), VariableImpl("X"))), FactImpl("male", List(VariableImpl("X"))))),
+    FactImpl("father", List(AtomImpl("abraham"), AtomImpl("isaac"))), FactImpl("father", List(AtomImpl("terach"), AtomImpl("abraham"))),
+    FactImpl("male", List(AtomImpl("isaac"))), FactImpl("male", List(AtomImpl("abraham"))), FactImpl("male", List(AtomImpl("terach"))))
+
+
+  val mulTheorySubstituted: List[Clause] = List(FactImpl("sum", List(VariableImpl("X"), AtomImpl(0), VariableImpl("X"))),
+    RuleImpl(FactImpl("sum", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), StructImpl("s", List(VariableImpl("Z"))))),
+      List(FactImpl("sum", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"))))),
+    FactImpl("mul", List(VariableImpl("X"), AtomImpl(0), AtomImpl(0))),
+    RuleImpl(FactImpl("mul", List(VariableImpl("X"), StructImpl("s", List(VariableImpl("Y"))), VariableImpl("Z"))),
+      List(FactImpl("mul", List(VariableImpl("X"), VariableImpl("Y"), VariableImpl("W"))), FactImpl("sum", List(VariableImpl("X"), VariableImpl("W"), VariableImpl("Z"))))))
+
+  val relTheorySubstituted: List[Clause] = List(RuleImpl(FactImpl("son", List(VariableImpl("X"), VariableImpl("Y"))),
+    List(FactImpl("father", List(VariableImpl("Y"), VariableImpl("X"))), FactImpl("male", List(VariableImpl("X"))))),
+    FactImpl("father", List(AtomImpl("abraham"), AtomImpl("isaac"))), FactImpl("father", List(AtomImpl("terach"), AtomImpl("abraham"))),
+    FactImpl("male", List(AtomImpl("isaac"))), FactImpl("male", List(AtomImpl("abraham"))), FactImpl("male", List(AtomImpl("terach"))))
+
 
   "A term list" should "retrieve all the variables it contains" in {
     assert(termList.getVariables == termListVariables)
