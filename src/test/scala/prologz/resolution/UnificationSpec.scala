f647ecfedf49fa8119b79ba5f2feca57e9b2faa6
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 12:07:29 +0200

    unification spec (to be completed)

--- src/test/scala/prologz/resolution/UnificationSpec.scala
+++ src/test/scala/prologz/resolution/UnificationSpec.scala
@@ -12,36 +12,45 @@ class UnificationSpec extends AnyFlatSpec with Matchers with Utils {
 
   private val mulTheoryFacts = mulTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
   private val mulTheoryRules = mulTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
+
   private val relTheoryFacts = relTheory.flatMap{ case fact: Fact => fact.some; case _ => None }
   private val relTheoryRules = relTheory.flatMap{ case rule: Rule => rule.some; case _ => None }
 
-  private val otherMulGoal = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0), VariableImpl("Y")))
-  private val otherMulGoalRes = Substitution(VariableImpl("Y") -> AtomImpl(0))
-  private val mulGoalAfterOther = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0)))
   private val mulGoalRes = Substitution(VariableImpl("Y") -> StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))))))))
-  private val otherMulGoalAfterFirst = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0), StructImpl("s", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0)))))))))
+  private val otherMulGoal = FactImpl("mul", List(StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), AtomImpl(0), VariableImpl("Y")))
+  private val otherMulGoalRes = Substitution(VariableImpl("X") -> StructImpl("s", List(StructImpl("s", List(AtomImpl(0))))), VariableImpl("Y") -> AtomImpl(0))
 
+  private val relGoalRes = Substitution(VariableImpl("X") -> AtomImpl("isaac"), VariableImpl("Y") -> AtomImpl("abraham"))
   private val otherRelGoal = FactImpl("father", List(VariableImpl("X"), VariableImpl("Y")))
   private val otherRelGoalRes = Substitution(VariableImpl("X") -> AtomImpl("abraham"), VariableImpl("Y") -> AtomImpl("isaac"))
-  private val relGoalAfterOther = FactImpl("son", List(VariableImpl("abraham"), VariableImpl("isaac")))
-  private val relGoalRes = Substitution(VariableImpl("X") -> AtomImpl("isaac"), VariableImpl("Y") -> AtomImpl("abraham"))
-  private val otherRelGoalAfterFirst = FactImpl("father", List(VariableImpl("isaac"), VariableImpl("abraham")))
 
-  "A theory fact" should "not unify to a goal with a different predicate name" in {
+  "A theory fact" should "not unify with a goal having a different predicate name" in {
     for(theory <- mulTheoryFacts; goal <- mulGoals if theory.name != goal.name) theory.unify(goal, Nil) shouldBe None
     for(theory <- relTheoryFacts; goal <- relGoals if theory.name != goal.name) theory.unify(goal, Nil) shouldBe None
   }
 
-  it should "not unify to a goal with a different arguments arity" in {
+  it should "not unify with a goal having a different arguments arity" in {
     for(theory <- mulTheoryFacts; goal <- mulGoals if theory.args.size != goal.args.size) theory.unify(goal, Nil) shouldBe None
     for(theory <- relTheoryFacts; goal <- relGoals if theory.args.size != goal.args.size) theory.unify(goal, Nil) shouldBe None
   }
 
-  it should "unify to a compatible goal retrieving the substitution and other goals after its application" in {
-    mulTheoryFacts
-
-
+  it should "unify with a goal having the same predicate name and all arguments that unify, retrieving the right substitution" in {
+    mulTheoryFacts(1).unify(otherMulGoal, Nil) shouldBe (otherMulGoalRes, Nil).some
+    relTheoryFacts.head.unify(otherRelGoal, Nil) shouldBe (otherRelGoalRes, Nil).some
   }
 
+  "A theory rule" should "not unify with a goal having a predicate name different from its head" in {
+    for(theory <- mulTheoryRules; goal <- mulGoals if theory.head.name != goal.name) theory.unify(goal, Nil) shouldBe None
+    for(theory <- relTheoryRules; goal <- relGoals if theory.head.name != goal.name) theory.unify(goal, Nil) shouldBe None
+  }
+
+  it should "not unify with a goal having an arguments arity different from its head" in {
+    for(theory <- mulTheoryRules; goal <- mulGoals if theory.head.args.size != goal.args.size) theory.unify(goal, Nil) shouldBe None
+    for(theory <- relTheoryRules; goal <- relGoals if theory.head.args.size != goal.args.size) theory.unify(goal, Nil) shouldBe None
+  }
+
+  it should "unify with a goal having the same predicate name of its head and all arguments that unify" in {
+
+  }
 
 }
