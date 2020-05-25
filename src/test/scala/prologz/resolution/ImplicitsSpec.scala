ca39dfeb1b261f061c0cc8a4a677ffdcf8051fda
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-25 19:40:09 +0200

    completed implicit spec implementation

--- src/test/scala/prologz/resolution/ImplicitsSpec.scala
+++ src/test/scala/prologz/resolution/ImplicitsSpec.scala
@@ -1,8 +1,10 @@
 package prologz.resolution
 
+import scalaz._
+import Scalaz._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import prologz.dsl.{AtomImpl, Fact, FactImpl, Rule, RuleImpl, StructImpl, Variable, VariableImpl}
+import prologz.dsl.{AtomImpl, FactImpl, Rule, RuleImpl, StructImpl, Variable, VariableImpl}
 import prologz.resolution.Implicits.{RichFact, RichFactList, RichRule, RichTermList}
 import prologz.utils.Utils
 
@@ -11,10 +13,10 @@ class ImplicitsSpec extends AnyFlatSpec with Matchers with Utils {
   private val variablesToRename: Set[Variable] = Set(VariableImpl("X"), VariableImpl("Y"),  VariableImpl("Z"))
   private val substitution = Substitution(VariableImpl("X") -> AtomImpl(1), VariableImpl("Y") -> StructImpl("s", List(VariableImpl("Z"))), VariableImpl("Z") -> VariableImpl("Y'"))
 
-  private val relRuleVariables = Set(VariableImpl("X"), VariableImpl("Y"))
   private val relRuleHeadVariables = Set(VariableImpl("X"), VariableImpl("Y"))
-  private val mulRuleVariables = Set(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"), VariableImpl("W"))
+  private val relRuleBodyVariables = Set(VariableImpl("Y"), VariableImpl("X"))
   private val mulRuleHeadVariables = Set(VariableImpl("X"), VariableImpl("Y"), VariableImpl("Z"))
+  private val mulRuleBodyVariables = Set(VariableImpl("X"), VariableImpl("Y"), VariableImpl("W"), VariableImpl("Z"))
 
   private val relRuleRenamed = RuleImpl(FactImpl("son", List(VariableImpl("X'"), VariableImpl("Y'"))),
     List(FactImpl("father", List(VariableImpl("Y'"), VariableImpl("X'"))), FactImpl("male", List(VariableImpl("X'")))))
@@ -57,27 +59,33 @@ class ImplicitsSpec extends AnyFlatSpec with Matchers with Utils {
   }
 
   "A fact list" should "retrieve all the variables it contains" in {
-   // assert(factList.getVariables == factListVariables)
+    mulTheory(3).asInstanceOf[Rule].body.getVariables shouldBe mulRuleBodyVariables
+    relTheory.head.asInstanceOf[Rule].body.getVariables shouldBe relRuleBodyVariables
   }
 
   it should "perform a renaming generating a correctly updated fact list" in {
-  //  assert(factList.rename(variablesToRename) == factListRenamed)
+    mulTheory(3).asInstanceOf[Rule].body.rename(variablesToRename) shouldBe mulRuleRenamed.body
+    relTheory.head.asInstanceOf[Rule].body.rename(variablesToRename) shouldBe relRuleRenamed.body
   }
 
   it should "perform a substitution generating a correctly updated fact list" in {
-//    assert(factList.substitute(substitution) == factListSubstituted)
+    mulTheory(3).asInstanceOf[Rule].body.substitute(substitution) shouldBe mulRuleSubstituted.body
+    relTheory.head.asInstanceOf[Rule].body.substitute(substitution) shouldBe relRuleSubstituted.body
   }
 
   "A rule" should "retrieve all the variables it contains" in {
-  //  assert(rule.getVariables == ruleVariables)
+    mulTheory(3).asInstanceOf[Rule].getVariables shouldBe mulRuleHeadVariables |+| mulRuleBodyVariables
+    relTheory.head.asInstanceOf[Rule].getVariables shouldBe relRuleHeadVariables |+| relRuleBodyVariables
   }
 
   it should "perform a renaming generating a correctly updated rule" in {
-    //assert(rule.rename(variablesToRename) == ruleRenamed)
+    mulTheory(3).asInstanceOf[Rule].rename(variablesToRename) shouldBe mulRuleRenamed
+    relTheory.head.asInstanceOf[Rule].rename(variablesToRename) shouldBe relRuleRenamed
   }
 
   it should "perform a substitution generating a correctly updated rule" in {
-  //  assert(rule.substitute(substitution) == ruleSubstituted)
+    mulTheory(3).asInstanceOf[Rule].substitute(substitution) shouldBe mulRuleSubstituted
+    relTheory.head.asInstanceOf[Rule].substitute(substitution) shouldBe relRuleSubstituted
   }
 
 }
