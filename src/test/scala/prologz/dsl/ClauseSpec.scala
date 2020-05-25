5d69b78e78850ab272aefad3b753c5dbd8b669ef
Tommaso Bombardi <tommaso.bombardi@studio.unibo.it>
2020-05-26 01:22:46 +0200

    added specification for clause and term tests

--- src/test/scala/prologz/dsl/ClauseSpec.scala
+++ src/test/scala/prologz/dsl/ClauseSpec.scala
@@ -1,7 +1,26 @@
 package prologz.dsl
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class ClauseSpec extends AnyFlatSpec {
+class ClauseSpec extends AnyFlatSpec with Matchers {
+
+  "The predicate factory" should "create a valid predicate in case of valid predicate name" in {
+
+  }
+
+  it should "retrieve an error list in case of errors in predicate name" in {
+
+  }
+
+  "The use of a predicate" should "create a valid fact in case of valid predicate and valid argument list" in {
+
+  }
+
+  it should "retrieve an error list in case of errors in predicate or in argument list" in {
+
+  }
+
+  // TO DO
 
 }
--- src/test/scala/prologz/dsl/TermSpec.scala
+++ src/test/scala/prologz/dsl/TermSpec.scala
@@ -1,7 +1,44 @@
 package prologz.dsl
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class TermSpec extends AnyFlatSpec {
+class TermSpec extends AnyFlatSpec with Matchers {
+
+  "The module for implicit term creation" should "covert a double into a valid constant term" in {
+
+  }
+
+  it should "convert an integer into a valid constant term" in {
+
+  }
+
+  it should "convert a string without errors and starting with a lowercase letter into a valid constant term" in {
+
+  }
+
+  it should "convert a string without errors and starting with an uppercase letter into a valid variable term" in {
+
+  }
+
+  it should "convert a string containing errors into an error list" in {
+
+  }
+
+  "The functor factory" should "create a valid functor in case of valid functor name" in {
+
+  }
+
+  it should "retrieve an error list in case of errors in functor name" in {
+
+  }
+
+  "The use of a functor" should "create a valid compound term in case of valid functor and valid argument list" in {
+
+  }
+
+  it should "retrieve an error list in case of errors in functor or in argument list" in {
+
+  }
 
 }
